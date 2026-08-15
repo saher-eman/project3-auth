e696dbbe630ad2f3c89bf8b5e2eabaa4112641db
Saher Eman <sahereman231@gmail.com>
2026-08-15 21:33:10 +0500

    Add users route to server.js
    Connect users API

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api', require('./routes/protected'));
+app.use('/api/users', require('./routes/users'));
 
 app.listen(process.env.PORT, () => {
   console.log('Server chal raha hai: http://localhost:3000');
